9056b3fcc36dc39f09ef5b7ca0346f067a18ceb2
Riley Kuhlman <rileyk42@icloud.com>
2018-03-30 17:29:44 -0500

    Added markers

--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
-function setup() {
-  createCanvas(400, 400);
-}
+mapboxgl.accessToken = "pk.eyJ1Ijoicmt1aGwiLCJhIjoiY2pmZWc4ZHZlMm9rajN1bXpnMDFqeWV5diJ9.IsGCAMUslYfZKzOEVjR7Jw";
 
-function draw() {
-  background(0);
-}
+/* Map: This represents the map on the page. */
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/dark-v9",
+  zoom: 0,
+  center: [0, 0]
+});
+
+map.on("load", function() {
+  /* Image: An image is loaded and added to the map. */
+  map.loadImage("https://i.imgur.com/MK4NUzI.png", function(error, image) {
+    if (error) throw error;
+    map.addImage("custom-marker", image);
+    /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
+    map.addLayer({
+      id: "markers",
+      type: "symbol",
+      /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
+      source: {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [{
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": ["121", "31"]
+              }
+          },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "Point",
+                "coordinates": ["0", "0"]
+              }
+          },
+        ]
+        }
+      },
+      layout: {
+        "icon-image": "custom-marker",
+      }
+    });
+  });
+});
