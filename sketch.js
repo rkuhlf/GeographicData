40c7508800b60bce6d446302ccea4db4a998d597
Riley Kuhlman <rileyk42@icloud.com>
2018-03-31 08:03:56 -0500

    removed unnecessary libraries

--- sketch.js
+++ sketch.js
@@ -51,7 +51,7 @@ function setup() {
     new mapboxgl.Marker(el)
       .setLngLat(marker.geometry.coordinates)
       .setPopup(new mapboxgl.Popup({
-          offset: 3000
+          offset: 25
         }) // add popups
         .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
       .addTo(map);
