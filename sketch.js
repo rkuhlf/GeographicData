c5064770529766b42e63048f0a09bdd67b54498d
Riley Kuhlman <rileyk42@icloud.com>
2018-03-31 07:28:24 -0500

    got new method working

--- sketch.js
+++ sketch.js
@@ -25,44 +25,46 @@ function setup() {
     var data = earthquakes[i].split(/,/);
     var latitude = data[1];
     var longitude = data[2];
-    addPoint(longitude, latitude);
+    if (!isNaN(parseFloat(latitude)) && !isNaN(parseFloat(longitude))) {
+      addPoint(longitude, latitude);
+    }
   }
 
-  map.on("load", function() {
-    /* Image: An image is loaded and added to the map. */
-    map.loadImage("Images/earthquake.png", function(error, image) {
-      if (error) throw error;
-      map.addImage("custom-marker", image);
-      /* Style layer: A style layer ties together the source and image and specifies how they are displayed on the map. */
-      map.addLayer({
-        id: "earthquakes",
-        type: "symbol",
-        /* Source: A data source specifies the geographic coordinate where the image marker gets placed. */
-        source: {
-          type: "geojson",
-          data: {
-            type: "FeatureCollection",
-            features: points
-          }
-        },
-        layout: {
-          "icon-image": "custom-marker",
-        }
-      });
-    });
+  var geojson = {
+    type: 'FeatureCollection',
+    features: points
+  };
+
+  geojson.features.forEach(function(marker) {
+
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .setPopup(new mapboxgl.Popup({
+          offset: 25
+        }) // add popups
+        .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+      .addTo(map);
   });
+
+  document.getElementsByClassName("marker");
 
 
   function addPoint(lon, lat) {
     points.push({
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [lon, lat]
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lon, lat]
       },
-      "properties": {
-        "title": lon + ", " + lat,
-        "description": lon + ", " + lat
+      properties: {
+        title: lat + ", " + lon,
+        description: ' '
       }
     });
   }
