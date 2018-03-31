d4c9474551320cfee4696948fe882d9d9ed6f086
Riley Kuhlman <rileyk42@icloud.com>
2018-03-31 07:57:55 -0500

    icons adjusted for earthquake magnitude

--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ var earthquakes;
 
 var points;
 
+var mult = 0.05;
+
 function preload() {
-  earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv");
+  earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.csv");
 
 }
 
@@ -25,8 +27,9 @@ function setup() {
     var data = earthquakes[i].split(/,/);
     var latitude = data[1];
     var longitude = data[2];
+    var magnitude = data[4];
     if (!isNaN(parseFloat(latitude)) && !isNaN(parseFloat(longitude))) {
-      addPoint(longitude, latitude);
+      addPoint(longitude, latitude, magnitude);
     }
   }
 
@@ -40,27 +43,28 @@ function setup() {
     // create a HTML element for each feature
     var el = document.createElement('div');
     el.className = 'marker';
+    var s = pow(10, marker.geometry.mag / 2) * mult;
+    el.setAttribute("style", "width:" + s + "px;height:" + s + "px");
 
 
     // make a marker for each feature and add to the map
     new mapboxgl.Marker(el)
       .setLngLat(marker.geometry.coordinates)
       .setPopup(new mapboxgl.Popup({
-          offset: 25
+          offset: 3000
         }) // add popups
         .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
       .addTo(map);
   });
 
-  document.getElementsByClassName("marker");
 
-
-  function addPoint(lon, lat) {
+  function addPoint(lon, lat, m) {
     points.push({
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [lon, lat]
+        coordinates: [lon, lat],
+        mag: m
       },
       properties: {
         title: lat + ", " + lon,
